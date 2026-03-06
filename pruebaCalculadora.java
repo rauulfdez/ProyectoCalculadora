9b4cfc6bcb93fedf621612f0d818305095d81d48
rauulfdez <rfmolina04@gmail.com>
2026-03-06 17:59:13 +0100

    Añadida clase pruebaCalculadora

---
+++ pruebaCalculadora.java
@@ -0,0 +1,6 @@
+public class pruebaCalculadora {
+    public static void main(String[] args) {
+        Calculadora miCalc = new Calculadora(10, 5);
+        System.out.println("El resultado de la suma es: " + miCalc.suma());
+    }
+}
